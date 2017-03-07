5dcccab3cfb39de59b6300f3e464465fa70878c7
Alex Radu <dragosthealx@gmail.com>
2017-03-07 14:20:53 +0000

    Added tests and main module

---
+++ limigrations/limigrations.py
@@ -0,0 +1,121 @@
+"""Lightweight migrations system for python / sqlite3."""
+import os
+import sys
+import time
+import sqlite3
+
+
+def connect_database(db_file=None):
+  """Connect to the database.
+
+  Returns:
+    The connection and a cursor
+  """
+  conn = sqlite3.connect(db_file)
+  c = conn.cursor()
+  return (conn, c)
+
+
+def migrate(db_file=None, migrations_dir=None):
+  """Run the migrations.
+
+  Read all the migrations in the migrations directory, and add them to
+  the migrations table if they are not already there. They are first inserted
+  with the status 'down'.
+  Then, for all migrations with status 'down', taken in chronological order,
+  call the 'up' method and set their status as 'up'.
+
+  Args:
+    db_file (str, optional): The path to the database file. If not provided,
+                             a new database will be created with the name
+                             'database.db'.
+    migrations_dir (str, optional): The path to the migrations directory. If
+                                    not provided, an empty directory will be
+                                    created with the name 'migrations'.
+  Returns:
+    True if a migration was run, False otherwise.
+  """
+  # Create required files if not existent
+  if db_file is None:
+    db_file = 'database.db'
+  if migrations_dir is None:
+    migrations_dir = 'migrations'
+    os.mkdir(migrations_dir)
+  # Connect to db
+  conn, c = connect_database(db_file)
+  # Create migrations table if not existent
+  c.execute('''CREATE TABLE IF NOT EXISTS migrations
+             (file text, status text, created_at datetime)''')
+  conn.commit()
+  # Get all migrations
+  query = "SELECT * FROM migrations"
+  migrations = [row[0] for row in c.execute(query)]
+  # Upload the new ones
+  for mig in os.listdir(config.DB_FILE):
+    if mig not in migrations:
+      c.execute("""INSERT INTO migrations
+                   VALUES (?, ?, ?)""",
+                (mig, 'down', time.strftime('%Y-%m-%d %H:%M:%S')))
+  conn.commit()
+  # Add migrations to import path
+  sys.path.append(migrations_dir)
+  # For any migration that is down, run it
+  migrations_run = 0
+  for row in c.execute("""SELECT * FROM migrations
+                          WHERE status='down'
+                          ORDER BY date(created_at) DESC"""):
+    # Run the up method
+    mig = __import__(row[0].split('.py')[0])
+    mig.up(conn, c)
+    # Modify it
+    c.execute("""UPDATE migrations SET status='up'
+                 WHERE file=?""", (row[0],))
+    conn.commit()
+    migrations_run += 1
+  # Return the boolean
+  return migrations_run > 0
+
+
+def rollback(db_file=None, migrations_dir=None):
+  """Roll back a migration.
+
+  Calls the 'down' method of the latest migration with status
+  'up'. If the migrations table does not exist, it is created.
+
+  Args:
+    db_file (str, optional): The path to the database file. If not provided,
+                             a new database will be created with the name
+                             'database.db'.
+    migrations_dir (str, optional): The path to the migrations directory. If
+                                    not provided, an empty directory will be
+                                    created with the name 'migrations'.
+  Returns:
+    True if a migration was rolled back, False otherwise.
+  """
+  # Create required files if not existent
+  if db_file is None:
+    db_file = 'database.db'
+  if migrations_dir is None:
+    migrations_dir = 'migrations'
+    os.mkdir(migrations_dir)
+    # No migrations, so nothing to rollback
+    return False
+  # Connect to db and get the latest 'up' migration
+  conn, c = connect_database(db_file)
+  c.execute("""SELECT * FROM migrations
+               WHERE status='up'
+               ORDER BY date(created_at) DESC
+               LIMIT 1""")
+  row = c.fetchone()
+  # If nothing to run, return False
+  if row is None:
+    return False
+  # Run the rollback
+  mig = __import__((row[0].split('.py'))[0])
+  mig.down(conn, c)
+  # Update it
+  c.execute("""UPDATE migrations SET status='down'
+                 WHERE file=?""", (row[0],))
+  conn.commit()
+  # Something was rolled back, so return True
+  return True
---
+++ tests/test.py
@@ -0,0 +1,81 @@
+"""Test the limigrations module."""
+import unittest
+import os
+import limigrations
+
+
+class TestLimigrations(unittest.TestCase):
+  """Test case for limigrations module.
+
+  Attributes:
+    db_file (str): The path to the database file.
+    migrations_dir (str): The path to the migrations directory.
+  """
+
+  def setUp(self):
+    """Called before each test."""
+    self.db_file = 'database.db'
+    self.migrations_dir = 'migrations'
+    if not os.path.isdir(self.migrations_dir):
+      os.mkdir(self.migrations_dir)
+
+  def tearDown(self):
+    """Called after every test."""
+    if os.path.isfile(self.migrations_dir + '/test_migration.py'):
+      os.unlink(self.migrations_dir + '/test_migration.py')
+    if os.path.isfile(self.db_file):
+      os.unlink(self.db_file)
+    if os.path.isdir(self.migrations_dir):
+      os.rmdir(self.migrations_dir)
+
+  def test_migrate(self):
+    """Test whether the migration system works."""
+    # Create a migration
+    with open(self.migrations_dir + "/test_migration.py", "w") as f:
+      f.write("""\
+def up(conn, c):
+  c.execute('''CREATE TABLE IF NOT EXISTS test
+               ('col1' text, 'col2' text)''')
+  conn.commit()
+  c.execute('''INSERT INTO test
+               VALUES (?, ?)''', ('lol', 'stuff'))
+  conn.commit()
+def down(conn, c):
+  pass""")
+    # Migrate
+    limigrations.migrate(self.db_file, self.migrations_dir)
+    # Test whether the table was created
+    conn, c = limigrations.connect_database(self.db_file)
+    c.execute("SELECT * FROM test LIMIT 1")
+    row = c.fetchone()
+    self.assertEqual('lol', row[0])
+    self.assertEqual('stuff', row[1])
+    # If successful, delete the table
+    c.execute("DROP TABLE IF EXISTS test")
+    conn.commit()
+
+  def test_migrate_rollback(self):
+    """Test whether the rollback works."""
+    # Create a migration
+    with open(self.migrations_dir + "/test_migration.py", "w") as f:
+      f.write("""\
+def up(conn, c):
+  c.execute('''CREATE TABLE IF NOT EXISTS test
+               ('col1' text, 'col2' text)''')
+  conn.commit()
+  c.execute('''INSERT INTO test
+               VALUES (?, ?)''', ('lol', 'stuff'))
+  conn.commit()
+def down(conn, c):
+  c.execute('''DROP TABLE IF EXISTS test''')
+  conn.commit()""")
+    # Migrate and then rollback
+    limigrations.migrate(self.db_file, self.migrations_dir)
+    limigrations.rollback(self.db_file, self.migrations_dir)
+    # Test whether rollback works
+    conn, c = limigrations.connect_database(self.db_file)
+    c.execute("""SELECT name FROM sqlite_master
+                 WHERE type='table' AND name='test'""")
+    self.assertEqual(len(c.fetchall()), 0)
+    c.execute("DROP TABLE IF EXISTS test")
+    conn.commit()
