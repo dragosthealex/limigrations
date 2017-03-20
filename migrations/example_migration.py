09d1caacf42cffdec08cac4e50a82cc1bf7e6e81
Alex Radu <dragosthealx@gmail.com>
2017-03-20 22:02:21 +0000

    changed indentation for ex migration

--- migrations/example_migration.py
+++ migrations/example_migration.py
@@ -4,14 +4,14 @@ from limigrations.migration import BaseMigration
 
 
 class Migration(BaseMigration):
-  """A migration for somehting."""
+    """A migration for somehting."""
 
-  def up(self, conn, c):
-    """Run when calling 'migrate'."""
-    # Do something with connection and cursor
-    pass
+    def up(self, conn, c):
+        """Run when calling 'migrate'."""
+        # Do something with connection and cursor
+        pass
 
-  def down(self, conn, c):
-    """Run when calling 'rollback'."""
-    # Do something with connection and cursor
-    pass
+    def down(self, conn, c):
+        """Run when calling 'rollback'."""
+        # Do something with connection and cursor
+        pass
